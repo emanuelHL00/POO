0b84caee608ccc00a318631ddb41b9744a0dbef4
emanuelHL00 <emanuel.hernandezl@uqvirtual.edu.co>
2025-02-14 21:55:03 -0500

    up

---
+++ Ejercicioss/src/main/java/co/edu/uniquindio/poo/Taller1.java
@@ -0,0 +1,28 @@
+package co.edu.uniquindio.poo;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Taller1 {
+    public static void main(String[] args){
+
+
+        class Estudiante{
+        public String identificacion;
+        public String nombres;
+        public String apellidos;
+        public String correo;
+        public String telefono;
+        public int edad;
+
+        public Estudiante (String identificacion, String nombres, String apellidos,
+                           String correo, String telefono, int edad){
+
+        }
+
+    }
+
+
+    }
+
+}
