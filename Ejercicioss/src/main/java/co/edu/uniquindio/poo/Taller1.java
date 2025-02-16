b17b37d163ee70ff002d8c345f4ded8aa6db7c11
emanuelHL00 <emanuel.hernandezl@uqvirtual.edu.co>
2025-02-16 09:22:47 -0500

    up

--- Ejercicioss/src/main/java/co/edu/uniquindio/poo/Taller1.java
+++ Ejercicioss/src/main/java/co/edu/uniquindio/poo/Taller1.java
@@ -1,13 +1,11 @@
 package co.edu.uniquindio.poo;
 
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Taller1 {
-    public static void main(String[] args){
 
 
-        class Estudiante{
+        public static class Estudiante{
         public String identificacion;
         public String nombres;
         public String apellidos;
@@ -15,13 +13,68 @@ public class Taller1 {
         public String telefono;
         public int edad;
 
+
         public Estudiante (String identificacion, String nombres, String apellidos,
                            String correo, String telefono, int edad){
+            this.identificacion = identificacion;
+            this.nombres = nombres;
+            this.apellidos = apellidos;
+            this.correo = correo;
+            this.telefono = telefono;
+            this.edad = edad;
 
         }
 
+
+            public String toString() {
+                return
+                        "identificacion = " + identificacion + '\n' +
+                        "nombres = " + nombres + '\n' +
+                        "apellidos = " + apellidos + '\n' +
+                        "correo = " + correo + '\n' +
+                        "telefono = " + telefono + '\n' +
+                        "edad = " + edad
+                        ;
+
+            }
     }
 
+    public static class Main{
+            public static void main(String[] args){
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Bienvenido al ingreso de estudiantes");
+
+                System.out.println("Ingresa la identificacion del estudiante: ");
+                String identificacion = scanner.nextLine();
+
+                System.out.println("Ingresa los nombre del estudiante: ");
+                String nombres = scanner.nextLine();
+
+                System.out.println("Ingresa los apellidos del estudiante: ");
+                String apellidos = scanner.nextLine();
+
+                System.out.println("Ingresa el correo del estudiante: ");
+                String correo = scanner.nextLine();
+
+                System.out.println("Ingresa el telefono del estudiante: ");
+                String telefono = scanner.nextLine();
+
+                System.out.println("Ingresa la edad del estudiante: ");
+                int edad = scanner.nextInt();
+
+                Estudiante estudiante = new Estudiante(identificacion,nombres,apellidos,correo,telefono,edad);
+
+                System.out.println("Estudiante registrado");
+                System.out.println(estudiante);
+
+
+
+
+
+
+
+            }
 
     }
 
